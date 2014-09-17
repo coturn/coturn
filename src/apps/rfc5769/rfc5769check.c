4dc822e3e15f87fef2dd487fad6ee8e193ee0107
mom040267 <mom040267@users.noreply.github.com>
2014-09-17 21:16:15 +0000

    constant timestamp in the samples

--- src/apps/rfc5769/rfc5769check.c
+++ src/apps/rfc5769/rfc5769check.c
@@ -76,9 +76,7 @@ static int check_oauth(void) {
 
 	const char mac_key[33] = "ZksjpweoixXmvn67534m";
 	const size_t mac_key_length=strlen(mac_key);
-	uint64_t token_timestamp0 = turn_time();
-	const uint64_t token_timestamp = token_timestamp0 << 16;
-	printf("key timestamp: %llu\n",(unsigned long long)token_timestamp);
+	const uint64_t token_timestamp = 92470300704768;
 	const uint32_t token_lifetime = 3600;
 
 	const char kid[33] = "2783466234";
