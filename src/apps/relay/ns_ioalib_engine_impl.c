cd35ab46afe1fed644225d74ef2d8c12ac75837d
mom040267 <mom040267@users.noreply.github.com>
2014-12-11 09:02:49 +0000

    tls code cleaning

--- src/apps/relay/ns_ioalib_engine_impl.c
+++ src/apps/relay/ns_ioalib_engine_impl.c
@@ -433,15 +433,15 @@ ioa_engine_handle create_ioa_engine(super_memory_t *sm,
 
 void set_ssl_ctx(ioa_engine_handle e,
 		SSL_CTX *tls_ctx_ssl23,
-		SSL_CTX *tls_ctx_v1_0,
+		SSL_CTX *tls_ctx_v1_0
 #if TLSv1_1_SUPPORTED
-		SSL_CTX *tls_ctx_v1_1,
+		 ,SSL_CTX *tls_ctx_v1_1
 #if TLSv1_2_SUPPORTED
-		SSL_CTX *tls_ctx_v1_2,
+		 ,SSL_CTX *tls_ctx_v1_2
 #endif
 #endif
 #if DTLSv1_SUPPORTED
-		SSL_CTX *dtls_ctx
+		 ,SSL_CTX *dtls_ctx
 #endif
 #if DTLSv1_2_SUPPORTED
 		,SSL_CTX *dtls_ctx_v1_2
