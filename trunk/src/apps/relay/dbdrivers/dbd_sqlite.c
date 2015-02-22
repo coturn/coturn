82cdad2bb5155e577949e6e4fe3037c0e1e99a1a
mom040267@gmail.com <mom040267@gmail.com@a3c88463-c8e2-e606-d8d5-275b8e57aef6>
2015-02-22 07:14:00 +0000

    default sqlite database schema fixed

--- trunk/src/apps/relay/dbdrivers/dbd_sqlite.c
+++ trunk/src/apps/relay/dbdrivers/dbd_sqlite.c
@@ -155,7 +155,7 @@ static void init_sqlite_database(sqlite3 *sqliteconnection) {
 		"CREATE TABLE turn_origin_to_realm (origin varchar(127),realm varchar(127),primary key (origin))",
 		"CREATE TABLE turn_realm_option (realm varchar(127) default '',	opt varchar(32),	value varchar(128),	primary key (realm,opt))",
 		"CREATE TABLE oauth_key (kid varchar(128),ikm_key varchar(256) default '',timestamp bigint default 0,lifetime integer default 0,hkdf_hash_func varchar(64) default '',as_rs_alg varchar(64) default '',as_rs_key varchar(256) default '',auth_alg varchar(64) default '',auth_key varchar(256) default '',primary key (kid))",
-		"CREATE TABLE admin_user (uname varchar(32), realm varchar(127), password varchar(127), primary key (uname))",
+		"CREATE TABLE admin_user (name varchar(32), realm varchar(127), password varchar(127), primary key (name))",
 		NULL
 	};
 
