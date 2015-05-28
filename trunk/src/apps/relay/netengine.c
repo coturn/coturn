a17e635e7dc1849f3244553675adf3379e5d81cc
mom040267@gmail.com <mom040267@gmail.com@a3c88463-c8e2-e606-d8d5-275b8e57aef6>
2015-05-28 06:27:32 +0000

    Issues 11 and 12 fixed.

--- trunk/src/apps/relay/netengine.c
+++ trunk/src/apps/relay/netengine.c
@@ -336,7 +336,7 @@ int add_relay_addr(const char* addr) {
 
 		size_t i = 0;
 		for(i=0;i<turn_params.relays_number;++i) {
-			if(!strcmp(turn_params.relay_addrs[turn_params.relays_number-1],sbaddr)) {
+			if(!strcmp(turn_params.relay_addrs[i],sbaddr)) {
 				return 0;
 			}
 		}
