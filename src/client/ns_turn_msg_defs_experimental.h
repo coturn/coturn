0752c18b4d5491951b6ffb3122c84aee8c6490f1
Oleg Moskalenko <mom040267@gmail.com>
2015-08-27 05:20:29 -0700

    MOBILITY-TICKET changed to 0x8030

--- src/client/ns_turn_msg_defs_experimental.h
+++ src/client/ns_turn_msg_defs_experimental.h
@@ -40,9 +40,7 @@
 
 /* Mobility ==>> */
 /* conflicts with third-party authorization ! 0x802E is used for third-party authorization now */
-#define STUN_ATTRIBUTE_MOBILITY_TICKET (0x8040)
-#define STUN_ATTRIBUTE_MOBILITY_EVENT (0x802)
-#define STUN_ATTRIBUTE_MOBILITY_SUPPORT (0x8000)
+#define STUN_ATTRIBUTE_MOBILITY_TICKET (0x8030)
 /* <<== Mobility */
 
 /* Bandwidth */
