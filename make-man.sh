2ff0150c9804c839a398801be79863b96d04875e
Oleg Moskalenko <mom040267@gmail.com>
2017-02-20 01:10:38 -0800

    man pages util fixed

--- make-man.sh
+++ make-man.sh
@@ -1,29 +1,21 @@
 #!/bin/sh
 
-pushd man/man1 >/dev/null 2>&1
-  rm -rf ~0/*.1
+mkdir -p man/man1
+rm -rf man/man1/*1
 
-  for _tMB in {admin,server,utils} ;
-    do
-      txt2man -s 1 -t TURN \
-        -I turn${_tMB} \
-        -I turnadmin \
-        -I turnutils \
-        -I turnutils_uclient \
-        -I turnutils_stunclient \
-        -I turnutils_rfc5769check \
-        -I turnutils_peer \
-        -I turnutils_natdiscovery \
-        -I turnutils_oauth \
-        -B "TURN Server" ~1/README.turn${_tMB} | \
-        sed -e 's/-/\\-/g' > turn${_tMB}.1 ;
-    done
+txt2man -s 1 -t TURN -I turnserver -I turnadmin -I turnutils -I turnutils_uclient -I turnutils_stunclient -I turnutils_rfc5769check -I turnutils_peer -I turnutils_natdiscovery -I turnutils_oauth -B "TURN Server" README.turnserver | sed -e 's/-/\\-/g' > man/man1/turnserver.1
 
-  for _tLNK in {uclient,peer,stunclient,natdiscovery,oauth} ;
-    do
-      ln -s turnutils.1 turnutils_${_tLNK}.1 ; 
-    done
+txt2man -s 1 -t TURN -I turnserver -I turnadmin -I turnutils -I turnutils_uclient -I turnutils_stunclient -I turnutils_rfc5769check -I turnutils_peer -I turnutils_natdiscovery -I turnutils_oauth -B "TURN Server" README.turnadmin | sed -e 's/-/\\-/g'> man/man1/turnadmin.1
 
-  ln -s turnserver.1 coturn.1 ;
+txt2man -s 1 -t TURN -I turnserver -I turnadmin -I turnutils -I turnutils_uclient -I turnutils_stunclient -I turnutils_rfc5769check -I turnutils_peer -I turnutils_natdiscovery -I turnutils_oauth -B "TURN Server" README.turnutils | sed -e 's/-/\\-/g' > man/man1/turnutils.1
 
-popd >/dev/null 2>&1
+cd man/man1
+
+ln -s turnutils.1 turnutils_uclient.1
+ln -s turnutils.1 turnutils_peer.1
+ln -s turnutils.1 turnutils_stunclient.1
+ln -s turnutils.1 turnutils_natdiscovery.1
+ln -s turnutils.1 turnutils_oauth.1
+ln -s turnserver.1 coturn.1
+
+cd ../..
