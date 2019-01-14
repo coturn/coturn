9379e52d65b2f0d1ad74f4e0c92abf0f41c0bcb8
Alexander Terczka <alex@mail.at>
2019-01-14 16:36:19 +0100

    strip white-spaces from config file lines end, use arg="value " syntax if you need space at the end

--- src/apps/relay/mainrelay.c
+++ src/apps/relay/mainrelay.c
@@ -1619,7 +1619,9 @@ static void read_config_file(int argc, char **argv, int pass)
 				if (!s[0])
 					continue;
 				size_t slen = strlen(s);
-				while (slen && ((s[slen - 1] == 10) || (s[slen - 1] == 13)))
+
+				// strip white-spaces from config file lines end, use arg="value " syntax if you need space at the end
+				while (slen && ((s[slen - 1] == 10) || (s[slen - 1] == 13) || (s[slen - 1] == ' ')))
 					s[--slen] = 0;
 				if (slen) {
 					int c = 0;
