2d12ad4f2957174920cb7048dd1bf4d41df2e69d
Serhii Charykov <laammaar@gmail.com>
2021-04-26 23:29:46 +0300

    Refactor code

--- src/apps/relay/dbdrivers/dbd_sqlite.c
+++ src/apps/relay/dbdrivers/dbd_sqlite.c
@@ -95,7 +95,7 @@ static void sqlite_unlock(int write)
 
 //////////////////////////////////////////////////
 
-static int _sqlite_init_multithreaded(void) {
+static int sqlite_init_multithreaded(void) {
 
 #if defined(SQLITE_CONFIG_MULTITHREAD)
 	if (sqlite3_threadsafe() > 0) {
@@ -114,10 +114,6 @@ static int _sqlite_init_multithreaded(void) {
 #endif
 
 	return 0;
-}
-
-static void sqlite_init_multithreaded(void) {
-    (void) _sqlite_init_multithreaded();
 }
 
 static int donot_print_connection_success = 0;
@@ -175,16 +171,15 @@ static void init_sqlite_database(sqlite3 *sqliteconnection) {
 	}
 }
 
-pthread_once_t sqlite_init_once = PTHREAD_ONCE_INIT;
-
 static sqlite3 * get_sqlite_connection(void) {
+	static pthread_once_t sqlite_init_once = PTHREAD_ONCE_INIT;
 
 	persistent_users_db_t *pud = get_persistent_users_db();
 
 	sqlite3 *sqliteconnection = (sqlite3 *)pthread_getspecific(connection_key);
 	if(!sqliteconnection) {
 		fix_user_directory(pud->userdb);
-        (void) pthread_once(&sqlite_init_once, sqlite_init_multithreaded);
+		(void) pthread_once(&sqlite_init_once, (void (*)(void))sqlite_init_multithreaded);
 		int rc = sqlite3_open(pud->userdb, &sqliteconnection);
 		if(!sqliteconnection || (rc != SQLITE_OK)) {
 			const char* errmsg = sqlite3_errmsg(sqliteconnection);
