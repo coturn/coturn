61163b5257c0a4472148c2ac2d9a16fc3ace5a90
0x34d <ajsinghyadav00@gmail.com>
2022-10-03 09:49:25 +0530

    update build.sh
    Signed-off-by: 0x34d <ajsinghyadav00@gmail.com>

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -1,35 +1,44 @@
 #!/bin/bash -eu
 
-ASan() {
-   echo "Using AddressSanitizer"
-   export CFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link"
-   export CXXFLAGS="$CFLAGS"
+build(){
+   export CFLAGS="$1"
+   export CXXFLAGS="$1"
    export LIB_FUZZING_ENGINE=-fsanitize=fuzzer
+
+   mkdir build && cd build/
+   cmake -DFUZZER=ON -DLIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE" ../../.
+   make -j$(nproc)
+
+   cd fuzzing/
+   unzip FuzzStun_seed_corpus.zip
+   unzip FuzzStunClient_seed_corpus.zip
+
+   mkdir FuzzStun_Corpus
+   mkdir FuzzStunClient_Corpus
 }
 
-UBSan() {
-   echo "Using UndefinedBehaviorSanitizer"
-   export CFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=array-bounds,bool,builtin,enum,float-divide-by-zero,function,integer-divide-by-zero,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,unsigned-integer-overflow,unreachable,vla-bound,vptr -fno-sanitize-recover=array-bounds,bool,builtin,enum,float-divide-by-zero,function,integer-divide-by-zero,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,unreachable,vla-bound,vptr -fsanitize=fuzzer-no-link"
-   export CXXFLAGS="$CFLAGS"
-   export LIB_FUZZING_ENGINE=-fsanitize=fuzzer
-}
-
-MSan() {
-   echo "Using MemorySanitizer"
-   export CFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=memory -fsanitize-memory-track-origins -fsanitize=fuzzer-no-link"
-   export CXXFLAGS="$CFLAGS"
-   export LIB_FUZZING_ENGINE=-fsanitize=fuzzer
+run(){
+   DIR=build/fuzzing
+   if [ $1 == '0' ]
+   then
+      ./$DIR/FuzzStun   $DIR/FuzzStun_Corpus/   $DIR/FuzzStun_seed_corpus
+   else
+      ./$DIR/FuzzStunClient   $DIR/FuzzStunClient_Corpus/   $DIR/FuzzStunClient_seed_corpus
+   fi   
 }
 
 if [ $1 == "ASan" ]
 then
-  ASan
+   build "-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link"
 elif [ "$1" == "UBSan" ]
 then
-  UBSan
+   build "-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=array-bounds,bool,builtin,enum,float-divide-by-zero,function,integer-divide-by-zero,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,unsigned-integer-overflow,unreachable,vla-bound,vptr -fno-sanitize-recover=array-bounds,bool,builtin,enum,float-divide-by-zero,function,integer-divide-by-zero,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,unreachable,vla-bound,vptr -fsanitize=fuzzer-no-link"
 elif [ "$1" == "MSan" ]
 then
-  MSan
+   build "-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=memory -fsanitize-memory-track-origins -fsanitize=fuzzer-no-link"
+elif [ "$1" == "Run" ]
+then
+   run $2
 else
-  echo "use: $0 ASan | UBSan | MSan"
+  echo "use: ./$0 ASan | UBSan | MSan | run 0 | run 1"
 fi
