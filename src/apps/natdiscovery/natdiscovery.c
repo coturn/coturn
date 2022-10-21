4aaddd912cdf4b18b4f5661d241da73b393b100d
Kang Lin <kl222@126.com>
2022-10-21 15:17:57 +0800

    Windows::NatDiscovery: Compile successfully

--- src/apps/natdiscovery/natdiscovery.c
+++ src/apps/natdiscovery/natdiscovery.c
@@ -28,12 +28,16 @@
 * SUCH DAMAGE.
 */
 
-#include <err.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#if defined(WINDOWS)
+#include <getopt.h>
+#else
 #include <unistd.h>
+#include <err.h>
+#endif
 
 #include "ns_turn_utils.h"
 #include "apputils.h"
@@ -627,6 +631,24 @@ int main(int argc, char **argv)
 	int first=1;
 	ioa_addr other_addr, reflexive_addr, tmp_addr, remote_addr, local_addr, local2_addr;
 
+#if defined(WINDOWS)
+	{
+		WORD wVersionRequested;
+		WSADATA wsaData;
+		int e;
+
+		/* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+		wVersionRequested = MAKEWORD(2, 2);
+
+		e = WSAStartup(wVersionRequested, &wsaData);
+		if (e != 0) {
+			/* Tell the user that we could not find a usable */
+			/* Winsock DLL.                                  */
+			TURN_LOG_FUNC(TURN_LOG_LEVEL_ERROR, "WSAStartup failed with error: %d\n", e);
+			return 1;
+		}
+	}
+#endif
 
 	set_logfile("stdout");
 	set_system_parameters(0);
