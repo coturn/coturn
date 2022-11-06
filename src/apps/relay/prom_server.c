85415f867ae0bc12e42ce44bda93c9584e1f1cfc
Gustavo Garcia <gustavogb@gmail.com>
2022-11-06 20:45:32 +0100

    Fix format

--- src/apps/relay/prom_server.c
+++ src/apps/relay/prom_server.c
@@ -146,14 +146,14 @@ void prom_set_finished_traffic(const char *realm, const char *user, unsigned lon
 }
 
 void prom_inc_allocation(SOCKET_TYPE type) {
-  if (turn_params.prometheus == 1){
-    prom_gauge_inc(turn_total_allocations, (const char*[]) {socket_type_name(type)});
+  if (turn_params.prometheus == 1) {
+    prom_gauge_inc(turn_total_allocations, (const char *[]){socket_type_name(type)});
   }
 }
 
 void prom_dec_allocation(SOCKET_TYPE type) {
-  if (turn_params.prometheus == 1){
-    prom_gauge_dec(turn_total_allocations, (const char*[]) {socket_type_name(type)});
+  if (turn_params.prometheus == 1) {
+    prom_gauge_dec(turn_total_allocations, (const char *[]){socket_type_name(type)});
   }
 }
 
