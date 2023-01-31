41d7c09d11a2c26a371717eb3ab3553e3805b99b
tyranron <tyranron@gmail.com>
2023-01-31 18:10:22 +0200

    Fix arguments expansion in `docker-entrypoint.sh`

--- docker/coturn/rootfs/usr/local/bin/docker-entrypoint.sh
+++ docker/coturn/rootfs/usr/local/bin/docker-entrypoint.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # If command starts with an option, prepend it with a `turnserver` binary.
 if [ "${1:0:1}" == '-' ]; then
   set -- turnserver "$@"
 fi
 
-exec "$@"
+expanded=()
+for i in "$@"; do
+  expanded+=("$(eval "echo $i")")
+done
+
+exec "${expanded[@]}"
