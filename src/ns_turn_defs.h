af8a057eacb545e86a9b5d057e33ece41e1a4b49
Pavel Punsky <eakraly@users.noreply.github.com>
2023-04-10 19:00:08 -0700

    Update version to 4.6.2 (#1174)
    Update version number, generate authors and changelog files
    
    Release notes (short summary of changes)
    ```
    - Make sure microhttpd starts using epoll if supported
    - Add sessioncount to prometheus metrics
    - Add STUN request/response/error prometheus counters
    - Cleanup logs on turnserver start
    - Fix duplicate stdout log output
    - Log threadId to logs to aid in multi-threaded debugging
    - Optional build info compiled into turnserver binary
    - Fix arguments expansion in `docker-entrypoint.sh`
    - Santise database connection strings before printing to log
    - Support Windows MSVC
    - Add configuration option for TLS 1.3 ciphersuites
    - Improve openssl3 and FIPS support
    - Use single SSL_CTX for TLS and DTLS support
    - Update openssl API use to non-deprecated version
    - Set string bytes to null to prevent random origin
    - Fix memory corruption on socket close
    - Fix packet backlog fifo that processed packets in reverse order in some scenarios
    - Fix off-by-one when terminating gcm_nonce
    - Fixes to Redis memleaks and socketleaks
    - Fix malformed response to mobility refresh request
    - Fuzzing support
    - Ignore raw UDP if no_udp is enabled
    - Better detect availability of SCTP protocol
    ```
    
    ---------
    
    Co-authored-by: tyranron <tyranron@gmail.com>

--- src/ns_turn_defs.h
+++ src/ns_turn_defs.h
@@ -31,7 +31,7 @@
 #ifndef __IOADEFS__
 #define __IOADEFS__
 
-#define TURN_SERVER_VERSION "4.6.1"
+#define TURN_SERVER_VERSION "4.6.2"
 #define TURN_SERVER_VERSION_NAME "Gorst"
 #ifndef TURN_SERVER_BUILD_INFO
 #define TURN_SERVER_BUILD_INFO ""
