coturn/coturn

commit 2a695ea8551e782169c495d9239b2a0388763879

Add warning and disable web admin if no-tls option used (#1256)
Fixes https://github.com/coturn/coturn/issues/1239

https to web ui freeze in browser if no_tls option used, because no tls
stuff initialized.
This PR add warning about this and comment aboute this in default config

src/apps/relay/mainrelay.c
@@ -3076,6 +3076,11 @@ int main(int argc, char **argv) {
     }
   }
 
+  if (use_web_admin && turn_params.no_tls) {
+    TURN_LOG_FUNC(TURN_LOG_LEVEL_WARNING, "CONFIG: WARNING: web-admin support not compatible witn --no-tls option.\n");
+    use_web_admin = 0;
+  }
+
   openssl_setup();
 
   int local_listeners = 0;

docker/coturn/turnserver.conf
@@ -723,6 +723,7 @@ cli-password=CHANGE_ME
 # Enable Web-admin support on https. By default it is Disabled.
 # If it is enabled it also enables a http a simple static banner page
 # with a small reminder that the admin page is available only on https.
+# Not supported if no-tls option used
 #
 #web-admin
 

examples/etc/turnserver.conf
@@ -725,6 +725,7 @@
 # Enable Web-admin support on https. By default it is Disabled.
 # If it is enabled it also enables a http a simple static banner page
 # with a small reminder that the admin page is available only on https.
+# Not supported if no-tls option used
 #
 #web-admin
 

commit 9dfe8d5128b04b6b92811a5b63dd9672742f42d1

Fix no-tls warning typo (#1426)
Trivial log warning typo introduced in #1256 corrected.

src/apps/relay/mainrelay.c
@@ -3170,7 +3170,7 @@ int main(int argc, char **argv) {
   }
 
   if (use_web_admin && turn_params.no_tls) {
-    TURN_LOG_FUNC(TURN_LOG_LEVEL_WARNING, "CONFIG: WARNING: web-admin support not compatible witn --no-tls option.\n");
+    TURN_LOG_FUNC(TURN_LOG_LEVEL_WARNING, "CONFIG: WARNING: web-admin support not compatible with --no-tls option.\n");
     use_web_admin = 0;
   }
 