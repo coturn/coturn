456e2e81b37bdcc5dab7826e8959aede79db75d0
ashamedbit <muralianiruddhan@gmail.com>
2024-03-02 17:15:58 -0500

    Fix memory leak in netengine.c (#1411)
    This is in response to issue #1366
    The clang static analyzer basically claims that there is a memory leak
    happening in `set_ssl_ctx` for the variable `args`. The leak is
    triggered when the base event `base` is NULL and the condition within
    `set_ssl_ctx` is not triggered. Therefore as a patch I am adding an else
    condition to free it. (It cannot be freed after the event is created by
    `event_new` because `args` can be invoked as argument for callback
    function later on)
    
    Please let me know if this patch is helpful :)
    
    ---------
    
    Co-authored-by: Pavel Punsky <eakraly@users.noreply.github.com>

--- src/apps/relay/netengine.c
+++ src/apps/relay/netengine.c
@@ -349,6 +349,10 @@ void set_ssl_ctx(ioa_engine_handle e, turn_params_t *params) {
     args->next = params->tls_ctx_update_ev;
     params->tls_ctx_update_ev = ev;
     TURN_MUTEX_UNLOCK(&turn_params.tls_mutex);
+  } else {
+    if (args) {
+      free(args);
+    }
   }
 }
 
