2937e3fff437b10299fa24de592ceabb9bba3de3
sgodin <sgodin@sipspectrum.com>
2024-06-27 14:57:13 -0400

    fix typo

--- src/apps/relay/userdb.c
+++ src/apps/relay/userdb.c
@@ -733,7 +733,7 @@ void release_allocation_quota(uint8_t *user, int oauth, uint8_t *realm) {
   TURN_LOG_FUNC(TURN_LOG_LEVEL_INFO, "Global turn allocation count decremented, now %ld\n", global_allocation_count);
 #else
   size_t cur_count = (size_t)InterlockedDecrement((volatile LONG *)&global_allocation_count);
-  TURN_LOG_FUNC(TURN_LOG_LEVEL_INFO, "Global turn allocation count decremented, now %ld\n", curr_count);
+  TURN_LOG_FUNC(TURN_LOG_LEVEL_INFO, "Global turn allocation count decremented, now %ld\n", cur_count);
 #endif
 }
 
