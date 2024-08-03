f5b0c73ac264547ae642359f05c50db81527e14b
jingjingxyk <zonghengbaihe521@qq.com>
2024-08-03 09:00:16 +0800

    fix  error: call to undeclared function 'THREAD_setup'

--- src/apps/relay/mainrelay.c
+++ src/apps/relay/mainrelay.c
@@ -3422,7 +3422,6 @@ int THREAD_cleanup(void) {
 static int THREAD_setup(void) { return 1; }
 int THREAD_cleanup(void) { return 1; }
 #endif /* OPENSSL_VERSION_NUMBER < OPENSSL_VERSION_1_1_0 */
-
 #else
 static int THREAD_setup(void) { return 1; }
 int THREAD_cleanup(void) { return 1; }
